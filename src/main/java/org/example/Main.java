87e28f0dcf017f96a5923b96d445e77f20812c52
Dmitry Vasilenko <Vasilenko_dima1811@mail.ru>
2025-11-22 12:04:59 +0300

    Добавил зависимости в pom, а также создал hibernate.cfg для настройки hibernate без spring

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,13 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+//В PostgreSQL уже есть тестовая БД - user_test, в ней есть сущность user, c параметрами:
+/*
+*   id SERIAL PRIMARY KEY
+*   name VARCHAR(100) NOT NULL
+*   email VARCHAR(100) UNIQUE NOT NULL
+*   age INTEGER
+*   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+*/
 public class Main {
     public static void main(String[] args) {
 
